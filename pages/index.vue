031570bf7e3a3d6a5cd9d148d2fcd001b7f21f29
m1raclee4u <64864409+m1raclee4u@users.noreply.github.com>
2023-02-07 02:33:32 +0300

    initial

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="web-app">
+    <product/>
+  </div>
+</template>
+
+<script>
+import product from '../components/product.vue'
+export default {
+  components: { product },
+  name: 'IndexPage'
+}
+</script>
