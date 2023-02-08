04efc775ef1771e186313d040e5c302b3e81d6b6
m1raclee4u <64864409+m1raclee4u@users.noreply.github.com>
2023-02-08 13:16:11 +0300

    push

--- components/header-component.vue
+++ components/header-component.vue
@@ -2,7 +2,8 @@
   <header>
     <div @click="$router.push('/')" class="logo"></div>
     <div @click="$router.push('/cart')" class="cart">
-      <i class="cartCounter">{{ $store.state.cart.products.length }}</i>
+        Товаров в корзине : {{ $store.state.cart.products.length }}
+      <!-- <i class="cartCounter">{{ $store.state.cart.products.length }}</i> -->
     </div>
   </header>
 </template>
@@ -34,27 +35,29 @@ header {
   background-image: url("https://sun9-13.userapi.com/impg/E7kr6rybiGuaTdXQiaR7a5bvdVsRY6YSRZQupg/QrRbq_910IE.jpg?size=640x640&quality=95&sign=00b1ff7b83f6657eb50b2acd5ad03187&type=album");
 }
 .cart {
-  width: 15px;
-  height: 13px;
-  position: relative;
-  background-size: cover;
-  background-position: center;
-  background-image: url("../assets/img/icons/cart.svg");
-  transition: 1s;
-  .cartCounter {
-    transition: 1s;
-    position: absolute;
-    width: 10px;
-    height: 10px;
-    top: 0;
-    margin-left: 8px;
-    margin-top: -4px;
-    border-radius: 50%;
-    background-color: #000;
-    text-align: center;
-    color: #fff;
-    font-size: 8px;
-    font-style: unset;
-  }
+    color: white;
+    font-size: 12px;
+//   width: 15px;
+//   height: 13px;
+//   position: relative;
+//   background-size: cover;
+//   background-position: center;
+//   background-image: url("../assets/img/icons/cart.svg");
+//   transition: 1s;
+//   .cartCounter {
+//     transition: 1s;
+//     position: absolute;
+//     width: 10px;
+//     height: 10px;
+//     top: 0;
+//     margin-left: 8px;
+//     margin-top: -4px;
+//     border-radius: 50%;
+//     background-color: #000;
+//     text-align: center;
+//     color: #fff;
+//     font-size: 8px;
+//     font-style: unset;
+//   }
 }
 </style>
